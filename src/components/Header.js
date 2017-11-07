f45e9cda4e7a3fdcd5dfed0c5d78cfc83c624eac
Peter Thomas <you@example.com>
2017-11-07 17:21:19 +0000

    styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,21 @@ import { Link } from "react-router-dom";
 export default ({user, loggedIn, logOut}) => (
 	<div className="page-header">
 		<div className="row">
-			<div className="col-xs-12 col-sm-8">
+			<div className="col-xs-8 col-sm-8">
 				<Link className="home-link" to="/">
 					<div className="logo"></div>
-		  			<h1 className="header-subtitle"><small>Coding Fellowship Preparation</small></h1>
 		  		</Link>
 			</div>
-			<div className="col-xs-8 col-sm-3">
-				<h4 className="header-loggedIn">{ loggedIn? 'Welcome:' : null}</h4>
-				<h4 className="header-loggedIn">{ loggedIn? user.get('username') : null}</h4>
+			<div className="col-xs-4 col-sm-4">
+				{loggedIn? <button className="btn btn-default pull-right btn-logout" onClick={ () => logOut() }>Log Out</button> : null}
 			</div>
-			<div className="col-xs-1 col-sm-1">
-				{loggedIn? <button className="btn btn-default" onClick={ () => logOut() }>Log Out</button> : null}
+		</div>
+		<div className="row">
+			<div className="col-xs-12 col-sm-8">
+				<h1 className="header-subtitle">Coding Fellowship Preparation</h1>
+			</div>
+			<div className="col-xs-12 col-sm-4">
+				{loggedIn? <h4 className="header-loggedIn pull-right">{ user.get('username') }</h4> : null }
 			</div>
 		</div>
 	</div>
