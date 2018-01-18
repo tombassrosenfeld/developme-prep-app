a5f4c3d0146e456c788dbd64e7ce26e539b1d636
Peter Thomas <you@example.com>
2018-01-18 10:28:02 +0000

    header restyled

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,6 +18,15 @@ export default ({user, loggedIn, logOut}) => (
 			<div className="col-xs-12 titles">
 				<h1 className="header-course">Coding Fellowship |</h1>
 				<h1 className="header-username">&nbsp;{loggedIn? user.get('username') : 'App' }</h1>
+				<h2 className="header-cohort">Cohort 10</h2>
+			</div>
+		</div>
+
+		<div className="row bottom-nav">
+			<div className="col-xs-12 modules">			
+				{loggedIn? (
+					<div className="bottom-nav-module">Preparation</div> 
+				) : null}
 			</div>
 		</div>
 	</div>
