daa7fe71143bf525bddc7119704914f23fca8be7
Peter Thomas <you@example.com>
2018-01-18 15:35:00 +0000

    state change so can work on topics Nav

--- src/data/initial.js
+++ src/data/initial.js
@@ -1,10 +1,18 @@
 import { Map, List } from "immutable";
 
 export default Map({
-	isLoaded: false,
-	loggedIn: false,
-	user: Map({ id: '', username: '', password: '', token: null }),
-	topics: List([1,2,3,4,5]),
+	isLoaded: true,
+	// isLoaded: false,
+	loggedIn: true,
+	// loggedIn: false,
+	topics: List([
+		Map({id: 1, title: 'Javascript', short_title: 'JS', description: 'Lorem ipsum', tasks: [], selected: false }),
+		Map({id: 1, title: 'Javascript', short_title: 'JS', description: 'Lorem ipsum', tasks: [], selected: false }),
+		Map({id: 1, title: 'Javascript', short_title: 'JS', description: 'Lorem ipsum', tasks: [], selected: false }),
+		]),
+	// topics: List([1, 2, 3, 4, 5]),
+	user: Map({ id: '', username: 'Peter Thomas', password: '', token: null }),
+	// user: Map({ id: '', username: '', password: '', token: null }),
 	userProgress: List([]),
 	errors: '',
 });
