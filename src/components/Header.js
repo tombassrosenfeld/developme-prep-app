f938d99e20e740b2e57ed8ff924c6c5a70331d04
Peter Thomas <you@example.com>
2018-01-22 10:43:50 +0000

    removed unnecessary markup from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,17 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 export default ({user, loggedIn, logOut}) => (
-	<div className="page-header">
-		<div className="top-nav">
-			<div className="row">
-				<div className="col-xs-12">
-					<Link className="home-link" to="/">
-						<div className="logo"></div>
-			  		</Link>	
-					{loggedIn? <button className="btn btn-default pull-right btn-logout" onClick={ () => logOut() }>Log Out</button> : null}
-				</div>
+	<div>
+		<div className="row top-nav">
+			<div className="col-xs-12">
+				<Link className="home-link" to="/">
+					<div className="logo"></div>
+		  		</Link>	
+				{loggedIn? <button className="btn btn-default pull-right btn-logout" onClick={ () => logOut() }>Log Out</button> : null}
 			</div>
 		</div>
 
-		<div className="row">
+		<div className="row header-body">
 			<div className="col-xs-12 titles">
 				<h1 className="header-course">Coding Fellowship |</h1>
 				<h1 className="header-username">&nbsp;{loggedIn? user.get('username') : 'App' }</h1>
