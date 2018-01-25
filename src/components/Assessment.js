eb777b302279be1eaa322c427dafdfd5223be641
Peter Thomas <you@example.com>
2018-01-25 11:19:56 +0000

    rendered the questions and answers

--- src/components/Assessment.js
+++ src/components/Assessment.js
@@ -6,10 +6,28 @@ export default ({ id, topicID, topics}) => (
 			<h1>{+id + 1}. { topics.getIn([topicID, 'assessments'])[id].assessment_title }</h1>
 	  		<h2>Questions</h2>
 	  	</div>
-	  	{ topics.getIn([topicID, 'assessments'])[id].questions.map( (question, i) => (
-		  	<div className="task-resources">
-		  		<p>{question.question}</p>
-		  	</div>
-		)) }
+	  	<form>
+			<div className="assessment-questions">
+			  	{ topics.getIn([topicID, 'assessments'])[id].questions.map( (question, i) => (
+					<div className="panel" key={i}>
+			  			<div className="row">
+			  				<div className="col-xs-12">
+			  					<p>{question.question}</p>
+			  					{ question.answer_type === 'Multiple Choice' ? 
+			  						question.answers.map( (answer, i) => (
+										<div className="radio">
+											<label>
+								            	<input type="radio" value="option1" checked={false} />
+										 		{answer.answer_choice}
+								         	</label>
+								        </div>
+			  						))
+			  					: null }
+			  				</div>
+			  			</div>
+				  	</div>
+				)) }
+			</div>
+		</form>
 	</div>
 )
