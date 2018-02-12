7559cc70fff7ec1b7e01ed370313e55b5d6c3f7b
Peter Thomas <you@example.com>
2018-02-12 09:26:21 +0000

    baseurl

--- src/data/axios.js
+++ src/data/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // create a version of axios with useful defaults
 export default axios.create({
-    baseURL: "http://developme.box", // TODO: Change this to live site...
+    baseURL: "http://resources.developme.box", // TODO: Change this to live site...
     // make sure we get JSON back
     headers: {"Accept": "application/json"},
 });
