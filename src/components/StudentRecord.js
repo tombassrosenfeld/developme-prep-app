35fe06020f19ab5a8b0136f3398aba1563ea039b
KyeBuff <kye.buffery@btinternet.com>
2018-02-20 11:06:39 +0000

    able to email each student, total tasks record displayed

--- src/components/StudentRecord.js
+++ src/components/StudentRecord.js
@@ -1,23 +1,40 @@
 import React from 'react';
 import AssessmentList from '../containers/AssessmentList';
-import ProgressBar from '../components/ProgressBar';
+import ProgressBar from './ProgressBar';
+import StudentStatus from './StudentStatus';
 
-export default ({student, totalTasks}) => (
-	<div className="col-xs-12 col-sm-8 col-md-6 narrow-padding">
-		<div className="topics panel">
-			<div className="topics-header">
-				<div className="topics-header-icon"><i className="fa fa-2x fa-file-o" aria-hidden="true"></i></div>
-			</div>
-	  		<h1 className="topic-title">{ 'Student: ' + student.get('name') }</h1>
-	  		<ProgressBar 
-	  			text={"Progress"}
-	  			value={student.get('progress').size} 
-					maxValue={totalTasks} 
-					progressPercentage={(student.get('progress').size / totalTasks * 100).toFixed(0) + '%'} 
-	  		/>
-	  </div>
-	  <div className="panel narrow-padding">
-	  </div>
-	</div>
-)
+export default ({student, totalTasks}) => {
+
+	const progress = student.get('progress').size;
+
+	return (
+		<div className="col-xs-12 col-sm-8 col-md-6 narrow-padding">
+			<div className="topics panel">
+				<div className="topics-header">
+					<div className="topics-header-icon"><i className="fa fa-2x fa-file-o" aria-hidden="true"></i></div>
+				</div>
+		  		<h1 className="topic-title">{ student.get('name')} - Student</h1>
+		  		<ProgressBar 
+		  			text={"Progress"}
+		  			value={progress} 
+						maxValue={totalTasks} 
+						progressPercentage={(progress / totalTasks * 100).toFixed(0) + '%'} 
+		  		/>
+	  		<div className="narrow-padding hyperlink">
+					<a href={'mailto:' + student.get('email')}>Email {student.get('name')}</a>
+		  	</div>
+		  </div>
+		  <StudentStatus progress={progress} notDone={totalTasks - progress} />
+		  <div class="topics panel narrow-padding">
+		  	<div class="row">
+					<div className="col-sm-8">
+						<h2 className="panel-title">Tasks</h2> 
+					</div>
+					<div className="col-sm-4">
+					</div>
+				</div>
+		  </div>
+		</div>
+	)
+}
 
---
+++ src/components/StudentStatus.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { calculateTopicStatus } from '../utilities/utilities';
+
+export default ({progress, notDone}) => (
+	<div className="topicStatus panel">
+		<div className="row">
+			<div className="col-xs-6"><h2 className="panel-title">Completed</h2></div>
+			<div className="col-xs-2">
+				<p className="topicStatus-stat topicStatus-done">
+				{progress}
+				</p>
+				<p className="topicStatus-label">DONE</p>
+			</div>
+			<div className="col-xs-2">
+				<p className="topicStatus-stat topicStatus-not-done">
+				{notDone}
+				</p>
+				<p className="topicStatus-label">NOT DONE</p>
+			</div>
+			<div className="col-xs-2">
+				<div className="marker topicStatus-marker">
+				{ notDone > 0 ? 
+					<i className="fa fa-times-circle-o" aria-hidden="true"></i> : 
+					<i className="fa fa-check-circle-o" aria-hidden="true"></i>
+				}
+				</div>
+			</div>
+		</div>		
+	</div>	
+)
