6229721346dae1ae50341a7b3891d66dd17f3a91
KyeBuff <kye.buffery@btinternet.com>
2018-02-23 09:08:16 +0000

    started work on matching answers to questions

--- src/components/AssessmentListItem.js
+++ src/components/AssessmentListItem.js
@@ -1,28 +1,35 @@
 import React from 'react';
 
-export default ({i, assessment, onClick, questions, studentAssessmentData, topicTitle}) => (
-	<div>
-		<div className="row task">
-			<div className="col-xs-12">
-	  		<p 
-	  			className="taskList-task-title"
-	  			onClick={() => onClick(assessment.get('assessment_title'))}
-	  		>
-	  		{i + 1}. {assessment.get('assessment_title')}
-	  		</p>
+export default ({i, assessment, onClick, questions, studentAssessmentData, topicTitle}) => {
+	const studentAssessmentsArrByTopic = studentAssessmentData.get(topicTitle),
+	studentAnswersForAssessment = studentAssessmentsArrByTopic.find((assessment, index) => i === index);
+
+	console.log(questions ? questions.toJS() : null);
+
+	return (
+		<div>
+			<div className="row task">
+				<div className="col-xs-12">
+		  		<p 
+		  			className="taskList-task-title"
+		  			onClick={() => onClick(assessment.get('assessment_title'))}
+		  		>
+		  		{i + 1}. {assessment.get('assessment_title')}
+		  		</p>
+				</div>
+			</div>
+			<div className="row">
+				{questions ? questions.map((question, i) => (
+					<div>
+						<div className="col-xs-10" key={i}>
+							<p>
+								<strong>{question.get('question')} </strong> 
+								{question.get('answers').reduce((studentAnswer, ans, i) => i)}
+							</p>
+						</div>
+					</div>
+				)) : null}
 			</div>
 		</div>
-		<div className="row">
-			{questions ? questions.map((question, i) => (
-				<div>
-					<div className="col-xs-10" key={i}>
-						<p>
-							<strong>{question.get('question')} </strong> 
-							{question.get('answers').find((a, i) => i + 1 === +question.get('correct_answer')).get('answer_choice')}
-						</p>
-					</div>
-				</div>
-			)) : null}
-		</div>
-	</div>
-)
+	)
+}
