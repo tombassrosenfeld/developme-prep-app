7b8c4bb822490b426d0c6a206d1eeb9c1f8cae9a
KyeBuff <kye.buffery@btinternet.com>
2018-03-26 12:50:23 +0100

    updated method of getting topic so nav can be ordered appropriately

--- src/components/Topic.js
+++ src/components/Topic.js
@@ -3,17 +3,17 @@ import TaskList from '../containers/TaskList';
 import TopicStatus from '../containers/TopicStatus';
 import AssessmentList from '../containers/AssessmentList';
 
-export default ({id, topics}) => (
+export default ({id, topic}) => (
 	<div className="col-xs-12 col-sm-8 col-md-6 narrow-padding">
 		<div className="topics panel">
 			<div className="topics-header">
 				<div className="topics-header-icon"><i className="fa fa-2x fa-file-o" aria-hidden="true"></i></div>
 			</div>
-	  		<h1 className="topic-title">{ topics.getIn([id, 'short_title']) } <small>{ topics.getIn([id, 'title']) }</small></h1>
-	  		<p className="topic-description">{ topics.getIn([id, 'description']) }</p>
+	  		<h1 className="topic-title">{ topic.get('short_title') } <small>{ topic.get('title') }</small></h1>
+	  		<p className="topic-description">{ topic.get('description') }</p>
 	  	</div>
 	  	<TaskList id={ id }/>
 	  	<AssessmentList id={ id }/>
-	  	<TopicStatus topic={ topics.getIn([id]) }/>
+	  	<TopicStatus topic={ topic }/>
 	</div>
 )
