b7edc69afde8476380e1daf359da04091522e130
KyeBuff <kye.buffery@btinternet.com>
2018-03-27 10:00:46 +0100

    added log in/log out tests

--- src/__tests__/test.reducer.js
+++ src/__tests__/test.reducer.js
@@ -1,9 +1,10 @@
 import reducer from '../data/reducer';
 import initial from '../data/initial';
 import {LOGOUT} from '../data/actions';
+import {UPDATE_CREDENTIALS} from '../data/actions_API';
 import expect from 'expect';
 
-describe('reducer', () => {
+describe('Basic reducer testing', () => {
   it('should return the initial state', () => {
   	expect(reducer(initial, {type: null})).toEqual(initial);
   });
@@ -12,6 +13,20 @@ describe('reducer', () => {
   	expect(reducer(initial, {type: LOGOUT})).toEqual(initial.set('loggedIn', false));
   });
 
+  it('should log the user in and update their credentials', () => {
+  	const action = {
+  		type: UPDATE_CREDENTIALS,
+  		data: {
+	  		token: 1,
+	  		user_display_name: 'test_user',
+	  		user_email: 'test@test.com',
+  		}
+  	}
+  	expect(reducer(initial, action))
+  		.toEqual(initial.set('loggedIn', true)
+  			.setIn(['user', 'user_email'], action.data.user_email)
+  			.setIn(['user', 'token'], action.data.token)
+  			.setIn(['user', 'user_display_name'], action.data.user_display_name)
+			);
+	})
 });
-
-
