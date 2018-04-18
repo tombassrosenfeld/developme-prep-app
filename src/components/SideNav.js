8b085c69a78ba5dfb5e169bf90be2875e07d8949
kaushiro <malibu_kimee@yahoo.com>
2018-04-18 13:37:33 +0100

    animated menu toggle using react-motion

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import SideNavItem from './SideNavItem';
+import {Motion, spring} from 'react-motion';
 
-class MenuToggle extends Component {
+class SideNav extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			toggle: false,
+			height: 0,
 		};
 		this.switch = this.switch.bind(this);
 	}
+
 	switch() {
 		const current = this.state.toggle;
 		this.setState({toggle: !current})
+		var element = document.getElementById("menu");
+		this.setState({ height: this.state.height === 0 ? 200 : 0 })
 		const hamburger = document.querySelector(".hamburger");
     	hamburger.classList.toggle("is-active");
 	}
@@ -21,32 +26,37 @@ class MenuToggle extends Component {
 			<nav>
 				<div className="navNarrow col-xs-12 col-sm-4 col-md-2 col-md-offset-2 narrow-padding">
 					<div className="topics panel">
-						<div className="topics-header toggle-div" onClick={this.switch}>
+						<div className="topics-header toggle-div">
 							<div className="topics-header-icon toggle-icon">
-								<button class="hamburger hamburger--collapse" type="button">
-								  <span class="hamburger-box">
-								    <span class="hamburger-inner"></span>
+								<button className="hamburger hamburger--collapse" type="button" onClick={this.switch}>
+								  <span className="hamburger-box">
+								    <span className="hamburger-inner"></span>
 								  </span>
 								</button>
 							</div>
 							<p className="toggle-p">{userRole === 'student' ? "Topics" : "Cohorts"} </p>
 						</div>
 
-						{this.state.toggle ? 
-						<div className="narrowLinks">
-						<h2 className="topics-title">{title}</h2>
-							{ navItems.map((navItem, i) => (
-								<SideNavItem 
-									key={ i } 
-									id={ userRole === 'student' ? navItem.get('id') : navItem.get('name') } 
-									title={ userRole === 'student' ? navItem.get('short_title') : navItem.get('name') }
-									selected={ navItem.get('selected') } 
-									onClick={ () => onClickIcon(userRole === 'student' ? navItem.get('id') : i) }
-									route={ userRole === 'student' ? '/prep/topic/' : '/cohort/'}
-								/>
-							)) }
-						</div>
-						: null }
+						
+						<Motion style={{ height: spring(this.state.height) }} >
+							{
+								({ height }) => <div id = 'menu' style={Object.assign({}, { height } )}>
+									<div className="narrowLinks">
+									<h2 className="topics-title">{title}</h2>
+										{ navItems.map((navItem, i) => (
+											<SideNavItem 
+												key={ i } 
+												id={ userRole === 'student' ? navItem.get('id') : navItem.get('name') } 
+												title={ userRole === 'student' ? navItem.get('short_title') : navItem.get('name') }
+												selected={ navItem.get('selected') } 
+												onClick={ () => onClickIcon(userRole === 'student' ? navItem.get('id') : i) }
+												route={ userRole === 'student' ? '/prep/topic/' : '/cohort/'}
+											/>
+										)) }
+									</div>
+								</div>
+							}
+						</Motion>
 
 					</div>
 				</div>
@@ -57,5 +67,5 @@ class MenuToggle extends Component {
 
 
 
-export default MenuToggle; 
+export default SideNav; 
 
