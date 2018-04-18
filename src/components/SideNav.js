0807b689167cce8c4869571ceb3eb1e726ce51da
kaushiro <malibu_kimee@yahoo.com>
2018-04-18 13:46:10 +0100

    created animated menu with a different package

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import SideNavItem from './SideNavItem';
-import {Motion, spring} from 'react-motion';
+import AnimateHeight from 'react-animate-height';
 
 class SideNav extends Component {
 	constructor(props) {
@@ -16,12 +16,14 @@ class SideNav extends Component {
 		const current = this.state.toggle;
 		this.setState({toggle: !current})
 		var element = document.getElementById("menu");
-		this.setState({ height: this.state.height === 0 ? 200 : 0 })
+		this.setState({ height: this.state.height === 0 ? 'auto' : 0 })
 		const hamburger = document.querySelector(".hamburger");
     	hamburger.classList.toggle("is-active");
 	}
 	render() {
 		const {title, navItems, onClickIcon, userRole} = this.props;
+		const { height } = this.state;
+
 		return (
 			<nav>
 				<div className="navNarrow col-xs-12 col-sm-4 col-md-2 col-md-offset-2 narrow-padding">
@@ -37,27 +39,24 @@ class SideNav extends Component {
 							<p className="toggle-p">{userRole === 'student' ? "Topics" : "Cohorts"} </p>
 						</div>
 
-						
-						<Motion style={{ height: spring(this.state.height) }} >
-							{
-								({ height }) => <div id = 'menu' style={Object.assign({}, { height } )}>
-									<div className="narrowLinks">
-									<h2 className="topics-title">{title}</h2>
-										{ navItems.map((navItem, i) => (
-											<SideNavItem 
-												key={ i } 
-												id={ userRole === 'student' ? navItem.get('id') : navItem.get('name') } 
-												title={ userRole === 'student' ? navItem.get('short_title') : navItem.get('name') }
-												selected={ navItem.get('selected') } 
-												onClick={ () => onClickIcon(userRole === 'student' ? navItem.get('id') : i) }
-												route={ userRole === 'student' ? '/prep/topic/' : '/cohort/'}
-											/>
-										)) }
-									</div>
+							<AnimateHeight
+								duration={ 500 }
+					        	height={ height }
+					        >	
+								<div className="narrowLinks">
+								<h2 className="topics-title">{title}</h2>
+									{ navItems.map((navItem, i) => (
+										<SideNavItem 
+											key={ i } 
+											id={ userRole === 'student' ? navItem.get('id') : navItem.get('name') } 
+											title={ userRole === 'student' ? navItem.get('short_title') : navItem.get('name') }
+											selected={ navItem.get('selected') } 
+											onClick={ () => onClickIcon(userRole === 'student' ? navItem.get('id') : i) }
+											route={ userRole === 'student' ? '/prep/topic/' : '/cohort/'}
+										/>
+									)) }
 								</div>
-							}
-						</Motion>
-
+							</AnimateHeight>				
 					</div>
 				</div>
 			</nav>
