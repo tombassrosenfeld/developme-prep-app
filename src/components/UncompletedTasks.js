1d2780310dc2252482eb8378d3417eb02b93fece
kaushiro <malibu_kimee@yahoo.com>
2018-04-23 15:48:47 +0100

    instead of doing an itemized list of tasks and assessments listed only the topic is displayed

--- src/components/UncompletedTasks.js
+++ src/components/UncompletedTasks.js
@@ -2,42 +2,23 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 export default ({userProgress, uncompletedTopics }) => {
-
 	return (
 		<div className="topics panel">
 			<div className="topics-header">
 				<div className="topics-header-icon"><i className="fa fa-2x fa-file" aria-hidden="true"></i></div>
 			</div>
-			<p className="topic-description">These are the next 2 topics you have left to complete:</p>
-
-	  		<ul className='uncompleted-tasks-list'>
+			<p className="uncompleted-topic-description">These are the next 2 topics you have left to complete:</p>
+			
+	  	
+	  		<div className='uncompleted-tasks-list'>
 		  		{uncompletedTopics.map((topic, i) => 
-		  			<li key={i} className='short-title'><h5>{topic.get('short_title')}</h5>
-		  				<p className='task-p'>-tasks:</p>
-		  				<ol>
-		  					<Link to={ '/prep/topic/' + topic.get('id') + '/task/' + i }>
-					  			{topic.get('tasks').map((taskItem, i)=>
-				  					<li key={i}>{taskItem.get('task')}</li>
-				  				)}	
-			  				</Link>
-			  				
-				  		</ol>
-				  		{topic.get('assessments').size > 0 ?
-				  			<div>
-						  		<p className='assessment-p'>-assessments:</p>
-						  		<ol>
-						  			<Link to={ '/prep/topic/' + topic.get('id') + '/assessment/' + i }>
-							  			{topic.get('assessments').map((assessmentItem, i)=>
-						  					<li key={i}>{assessmentItem.get('assessment_title')}</li>
-						  				)}
-					  				</Link>
-						  		</ol>
-					  		</div>
-					  		: null
-				  		}
-				  	</li>
+		  			<div key={i} className='topics-icon row uncompleted-tasks-list-item'>
+			  			<Link to={ '/prep/topic/' + topic.get('id') }>
+			  				<h5>{topic.get('short_title')}</h5>
+			  			</Link>		
+				  	</div>
 		  		)}
-	  		</ul>
+	  		</div>
 
 	  	</div>
 	);
--- src/containers/UncompletedTasks.js
+++ src/containers/UncompletedTasks.js
@@ -6,13 +6,39 @@ const mapStateToProps = state => {
 	let userProgress = state.get('userProgress');
 	let completedShortTitles = userProgress.map(el=>el.split('.')[0]); 
 	let topics = state.get('topics');
-	let uncompletedTopicsArray = topics.filter(topic => completedShortTitles.reduce((isComplete, shortTitle) => shortTitle !== topic.get('short_title'), false));
-	console.log(uncompletedTopicsArray.toJS());
-	var uncompletedTopics = uncompletedTopicsArray.slice(0, 2);
-	console.log(uncompletedTopics.toJS());
+
+	let tasks = topics.map(topic => topic.get('tasks').size);
+	let assessments = topics.map(topic => topic.get('assessments').size);
+	let lengthArray = tasks.map((num, i)=> num + assessments.get(i));
+
+	// OBJECT FOR EACH TOPIC AND ITS LENGTH OF TASKS+ASSESSMENTS
+	let titles = topics.map(topic => topic.get('short_title'));
+	let lengthObject = {};
+	titles.forEach((key,i)=>lengthObject[key] = lengthArray.get(i));
+
+	// OBJECT FOR WHICH TOPICS AND HOW MANY OF ITS TASKS+ASSESSMENTS WAS COMPLETED
+	let completedObject = completedShortTitles.reduce((obj, title) => {
+		obj[title] = obj[title] + 1 || 1;
+		return obj;
+	}, {});
+
+	// OBJECT FOR TOPICS WHERE THE USER HAS NOT COMPLETED ALL ITS TASKS+ASSESSMENTS
+	let completedAllTasksObject = (obj1, obj2) => {
+		return Object.keys(obj1).filter(function(key){
+			return obj1[key] !== obj2[key];
+		});
+	}
+	let uncompletedTopicsArray = completedAllTasksObject(lengthObject, completedObject);
+
+	// TOPICS ARE FILTERED BASED ON TOPICS THAT AREN'T COMPLETED
+	const uncompletedTopics = topics.filter(topic => uncompletedTopicsArray.find(incompleteTopic => topic.get('short_title') === incompleteTopic));
+
+	// LIMITING THE NUMBER OF TOPICS SHOWN TO 3
+	const shortenedUncompletedTopicsArray = uncompletedTopics.slice(0, 2);
+
 	return {
 		userProgress: userProgress,
-	    uncompletedTopics: uncompletedTopics,
+	    uncompletedTopics: shortenedUncompletedTopicsArray,
 	}
 }
 	
