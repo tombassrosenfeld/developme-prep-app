7cb17249a24a924c476b98116aad791e07a980d7
Peter Thomas <you@example.com>
2018-05-31 10:59:39 +0100

    fix bootstrap markup for instructor view

--- src/components/instructor/AssessmentListItem.js
+++ src/components/instructor/AssessmentListItem.js
@@ -6,55 +6,61 @@ export default ({i, assessment, onClick, questions, studentAssessmentData, topic
 				attemptsForAssessment = studentAnswersForAssessment ? studentAnswersForAssessment.get('attempts') : null;
 
 	return attemptsForAssessment ? (
-		<div className="row assessment-item">
-			<div className="col-xs-8 task">
-	  		<p 
-	  			className="taskList-task-title"
-	  			onClick={() => onClick(assessment.get('assessment_title'))}
-	  		>
-	  		<strong>{i + 1}</strong>. {assessment.get('assessment_title')}
-	  		</p>
+		<div>
+			<div className="row assessment-item">
+				<div className="col-xs-8 task">
+			  		<p 
+			  			className="taskList-task-title"
+			  			onClick={() => onClick(assessment.get('assessment_title'))}
+			  		>
+			  			<strong>{i + 1}</strong>. {assessment.get('assessment_title')}
+			  		</p>
+				</div>
+				<div className="col-xs-4">
+		  			<p className="attempts">Attempts: {attemptsForAssessment}</p>
+				</div>
 			</div>
-			<div className="col-xs-4">
-	  			<p className="attempts">Attempts: {attemptsForAssessment}</p>
+			<div className="row">
+				{questions ? 
+				<div className="col-xs-12">
+		  			<p className="panel-title questions-header">Questions</p>
+				</div> : null }
 			</div>
-			{questions ? 
-			<div>
-	  		<p className="panel-title questions-header">Questions</p>
-			</div> : null }
-			<div className="col-xs-12">
-				{questions ? questions.map((question, i) => {
-					const answers = question.get('answers').toJS(),
-								correctAnswer = +question.get('correct_answer');
-					//If studentAnswers does not have a value at the index, then do no not run this code. 
-					//+ answers.length - studentAnswers.length - is used as studentAnswers[i] can be 0, which returns false.
-					return studentAnswers[i] + answers.length - studentAnswers.length || answers.length === studentAnswers.length ? (
-						<div className="row question-answer" key={question.get('question')}>
-							<div className="col-xs-10 question">
-								<p>
-									<strong>{i+1}: </strong> 
-									{question.get('question')}
-								</p>
-								<p><strong>Student's answer: </strong>{answers[studentAnswers[i]].answer_choice}</p>
-								<p><strong>Correct answer: </strong>{answers[correctAnswer-1].answer_choice}</p>
-							</div>
-							<div className="col-xs-2" key={i}>
-								<div className="marker topicStatus-marker">
-									{correctAnswer === studentAnswers[i]+1 ? 
-									<i className="fa fa-check-circle-o" aria-hidden="true"></i> : 
-									<i className="fa fa-times-circle-o" aria-hidden="true"></i>
-									}
+			<div className="row">
+				<div className="col-xs-12">
+					{questions ? questions.map((question, i) => {
+						const answers = question.get('answers').toJS(),
+									correctAnswer = +question.get('correct_answer');
+						//If studentAnswers does not have a value at the index, then do no not run this code. 
+						//+ answers.length - studentAnswers.length - is used as studentAnswers[i] can be 0, which returns false.
+						return studentAnswers[i] + answers.length - studentAnswers.length || answers.length === studentAnswers.length ? (
+							<div className="row question-answer" key={question.get('question')}>
+								<div className="col-xs-10 question">
+									<p>
+										<strong>{i+1}: </strong> 
+										{question.get('question')}
+									</p>
+									<p><strong>Student's answer: </strong>{answers[studentAnswers[i]].answer_choice}</p>
+									<p><strong>Correct answer: </strong>{answers[correctAnswer-1].answer_choice}</p>
+								</div>
+								<div className="col-xs-2" key={i}>
+									<div className="marker topicStatus-marker">
+										{correctAnswer === studentAnswers[i]+1 ? 
+										<i className="fa fa-check-circle-o" aria-hidden="true"></i> : 
+										<i className="fa fa-times-circle-o" aria-hidden="true"></i>
+										}
+									</div>
 								</div>
 							</div>
-						</div>
-					) : null;
-				}) : null}
+						) : null;
+					}) : null}
+				</div>
 			</div>
 		</div>
 	) : <div className="row">
-				<div className="col-xs-12">
-					<p>This student has not taken any of the assessments for this topic yet.</p>
-				</div>
+			<div className="col-xs-12">
+				<p>This student has not taken any of the assessments for this topic yet.</p>
 			</div>
+		</div>
 			
 }
