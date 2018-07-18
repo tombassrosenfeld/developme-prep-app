5f2259d2a3a0de18d8f4cd4ce59f55b2f556768e
Kye Buffery <kyebuff@Kyes-MacBook-Pro.local>
2018-07-18 16:31:33 +0100

    added gap

--- src/components/Topic.js
+++ src/components/Topic.js
@@ -5,7 +5,7 @@ import AssessmentList from '../containers/AssessmentList';
 const Parser = require('html-react-parser');
 
 export default ({id, topic}) => (
-	<div className="col-xs-12 col-sm-8 col-md-6 narrow-padding">
+	<div className="col-xs-12 col-sm-8 col-md-6 narrow-padding single-topic">
 		<div className="topics panel">
 			<div className="topics-header">
 				<div className="topics-header-icon"><i className="fa fa-2x fa-file" aria-hidden="true"></i></div>
