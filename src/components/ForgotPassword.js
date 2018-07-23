fbe90391c7a7a8039cb1bedb0c29977fd1ae6b4b
Kye Buffery <kyebuff@Kyes-MacBook-Pro.local>
2018-07-23 11:40:55 +0100

    moved var inside component

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import {baseURL} from '../data/axios';
 
-const resetLink = "wp-login.php?action=lostpassword&redirect_to="+baseURL+"wp-content%2Fthemes%2Fdev-me-resources-child%2Fapp-redirect.php";
-
-export default ({toggleForgot, showForgot, resetSuccess, forgotPasswordMessage}) => (
-	<div className="row">
+export default ({toggleForgot, showForgot, resetSuccess, forgotPasswordMessage}) => {
+	const resetLink = "wp-login.php?action=lostpassword&redirect_to="+baseURL+"wp-content%2Fthemes%2Fdev-me-resources-child%2Fapp-redirect.php";
+	return <div className="row">
 		<div className="col-xs-12">
 			<a className="forgot-password-link" href={baseURL+resetLink} target="_blank">Reset Password</a>
 		</div>
 	</div>
-);
+};
