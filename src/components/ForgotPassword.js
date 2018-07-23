85504b14f5ea3ef197b55ea020220b5eb36d1cc9
Kye Buffery <kyebuff@Kyes-MacBook-Pro.local>
2018-07-23 12:15:15 +0100

    reset redirect fixed

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {baseURL} from '../data/axios';
 
 export default ({toggleForgot, showForgot, resetSuccess, forgotPasswordMessage}) => {
-	const resetLink = "wp-login.php?action=lostpassword&redirect_to="+baseURL+"wp-content%2Fthemes%2Fdev-me-resources-child%2Fapp-redirect.php";
+	const resetLink = "wp-login.php?action=lostpassword";
 	return <div className="row">
 		<div className="col-xs-12">
 			<a className="forgot-password-link" href={baseURL+resetLink} target="_blank">Reset Password</a>
