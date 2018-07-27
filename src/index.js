932b8987681a22a34c73c05154ac68195249a57d
Kye Buffery <kyebuff@Kyes-MacBook-Pro.local>
2018-07-27 15:15:19 +0100

    removed warnings

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
-import {Map} from 'immutable';
 import { createStore, compose } from "redux";
 import {
   combineReducers
