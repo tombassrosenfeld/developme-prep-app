07c486cc9b28df52733cddb2955fb07b4a7a335b
Peter Thomas <you@example.com>
2018-07-30 16:41:44 +0100

    complete!

--- src/components/ModulesNav.js
+++ src/components/ModulesNav.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-export default ({userRole, navItems, hasMarking, hasNewFeedback}) => {
-	return (
-		<div className="row bottom-nav">
-			<div className="col-xs-12 modules">		
-				{userRole? navItems.get(userRole).map((navItem, i) => (
-					<Link to={navItem.get('link')} key={i}>
-						<div className={
-							hasMarking && navItem.get('label') == 'Marking' ? 'glow bottom-nav-module' : 'bottom-nav-module'}>
-							{navItem.get('label')}
-						</div> 
-		  			</Link>		
-				)) : null}
-			</div>
+export default ({userRole, navItems, hasMarking, hasNewFeedback}) => (
+	<div className="row bottom-nav">
+		<div className="col-xs-12 modules">		
+			{userRole? navItems.get(userRole).map((navItem, i) => (
+				<Link to={navItem.get('link')} key={i}>
+					<div className={ (hasMarking || hasNewFeedback) && (navItem.get('label') == 'Marking' || navItem.get('label') == 'Feedback') ? 'glow bottom-nav-module' : 'bottom-nav-module' }>
+						{navItem.get('label')}
+					</div> 
+	  			</Link>		
+			)) : null}
 		</div>
-	)
-}
+	</div>
+)
