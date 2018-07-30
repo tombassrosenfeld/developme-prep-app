996c256c071329516747a20540c6c5689850330d
Peter Thomas <you@example.com>
2018-07-30 14:29:18 +0100

    instructor path to new feedback

--- src/components/instructor/TopicListItem.js
+++ src/components/instructor/TopicListItem.js
@@ -4,11 +4,9 @@ import Completed from '../Completed';
 export default ({title, i, onClick, totalTasks, topicProgress, hasMarking}) => (
 	<div className="row task">
 		<div className="col-xs-9">
-			<p 
-				className="taskList-task-title"
-				onClick={onClick}
-			>
-			<strong>{i + 1}.</strong> {title} {hasMarking ? 'markme' : null}
+			<p className="taskList-task-title" onClick={onClick}>
+				<strong>{i + 1}.</strong> {title + ' '} 
+				{hasMarking ? <span className="marking-notice">*marking</span> : null}
 			</p>
 		</div>
 		<div className="col-xs-3">
