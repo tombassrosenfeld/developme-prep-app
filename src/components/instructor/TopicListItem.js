f3f191ae25b2aa198ce5afbca41bc17a621dccfd
Peter Thomas <you@example.com>
2018-07-30 12:56:09 +0100

    show which topics need marking in task list

--- src/components/instructor/TopicListItem.js
+++ src/components/instructor/TopicListItem.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import Completed from '../Completed';
 
-export default ({title, i, onClick, totalTasks, topicProgress}) => {
-	return (
-		<div className="row task">
-			<div className="col-xs-9">
-				<p 
-					className="taskList-task-title"
-					onClick={onClick}
-				>
-				<strong>{i + 1}.</strong> {title}
-				</p>
-			</div>
-			<div className="col-xs-3">
-				<Completed completed={totalTasks === topicProgress} />
-			</div>
+export default ({title, i, onClick, totalTasks, topicProgress, hasMarking}) => (
+	<div className="row task">
+		<div className="col-xs-9">
+			<p 
+				className="taskList-task-title"
+				onClick={onClick}
+			>
+			<strong>{i + 1}.</strong> {title} {hasMarking ? 'markme' : null}
+			</p>
 		</div>
-	)
-}
+		<div className="col-xs-3">
+			<Completed completed={totalTasks === topicProgress} />
+		</div>
+	</div>
+)
