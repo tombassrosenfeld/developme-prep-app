8c33f1da6095c5a0bf359e25f363de1f94088a3e
Kye Buffery <kyebuffery14@gmail.com>
2019-01-07 12:48:57 +0000

    added env file with slackToken

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -1,5 +1,6 @@
 import { fromJS } from "immutable";
 import { Events, scroller } from 'react-scroll';
+import env from '../env';
 
 export const preventDefault =  (e) => e.preventDefault();
 
@@ -103,6 +104,6 @@ export const sendSlackNotification = (username) => {
 	    .then(response => response.json())
 	};
 
-	return postData('https://hooks.slack.com/services/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', message)
+	return postData(`https://hooks.slack.com/services/TB2USJTQX/BBZEQ6V25/${env.slackToken}`, message)
 		.then(data => console.log(data))
 }	
